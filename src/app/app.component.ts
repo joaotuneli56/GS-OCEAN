557ef7ab249043d1cb4d388c1c968f1273db0e32
gabrielbaron01 <gabrielreisbaron@hotmail.com>
2024-06-08 23:10:05 -0300

    Estilização do site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { OceanConditionsComponent } from './components/ocean-conditions/ocean-conditions.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,OceanConditionsComponent],
+  imports: [RouterOutlet,OceanConditionsComponent,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import path from 'node:path';
+import { HomeComponent } from './components/home/home.component';
+import { OceanConditionsComponent } from './components/ocean-conditions/ocean-conditions.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component:HomeComponent},
+  {path: 'oceanproject', component:OceanConditionsComponent},
+  {path: '**', component:HomeComponent}
+];
